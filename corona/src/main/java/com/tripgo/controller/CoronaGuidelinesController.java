7b5b67d650d7130fc5aebb42c1e3df9dc7a5ee60
srinudevo <usraju.mpr@gmail.com>
2023-02-28 20:33:56 +0530

    initial commit

---
+++ corona/src/main/java/com/tripgo/controller/CoronaGuidelinesController.java
@@ -0,0 +1,26 @@
+package com.tripgo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoronaGuidelinesController {
+	@Value("${guidelines.oxygenLevels}")
+	private String oxygenLevels;
+	@Value("${guidelines.quarantine}")
+	private String quarantine;
+	@Value("${guidelines.liters}")
+	private String liters;
+	@Value("${guidelines.temparatureLevels}")
+	private String temparatureLevels;
+	
+	@GetMapping("/guidelines")
+	public String showCoronaGuidelines(Model model) {
+		
+		
+		return "guidelines [oxygenLevels: "+oxygenLevels + ", quarantine days: "+ quarantine + ", liters : "+ liters + ", temparatureLevels: " + temparatureLevels + "]";
+	}
+
+}
